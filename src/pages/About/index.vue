98e0661004efda652975cbf6877d9401838f3803
Simran <simran.abbasov.00@mail.ru>
2023-07-27 11:56:11 +0400

    about

--- src/pages/About/index.vue
+++ src/pages/About/index.vue
@@ -1,9 +1,126 @@
 <template>
     <main>
         <div class="container">
-            <h1>About</h1>
-            <p>Tron Genesis Mining Cloud şirketi, İşvecre'de 05.10.2021 tarihinde kurulmuş olan Tron madencilik şirketidir.  Şirketimizin amacı, Tron blok zincirindeki Gwei ücretini en aza indirmektir.  Bunun için çok çalışıyoruz. Kısa adıyla TRONGMC firması yepyeni bir plana imza atıyor!.  20.07.2023 tarihinde şirketimizin online bulut platformu ile karşınızdayız. Şirketimizin amacı, insanların Mining Online için makine kiralaması ve Tron blok zincirini desteklemesidir.  Bununla Gwei, ücretleri en aza indirecek ve daha az ücretle daha hızlı bir blok zinciri oluşturacak! Bunda kiraladığınız makineler size ek gelir sağlayacaktır.  Örneğin Vip1 satın aldığınızda özel yazılım şirketimizde bulunan X marka makineyi sisteminize hazırlıyor olacaktır.  Böylece Cloud Mining ile çevrimiçi olarak madencilik yapabileceksiniz. Promosyon ödüllerini sizin için daha karlı hale getirdik.  böylece şirketimize daha fazla müşteri getirerek, bizi ve kendinizi desteklemiş olacaksınız. Kendinize ve bize güvenerek kar edebilirsiniz, Ve firmamızın sunduğu VIP paketlerle işe başlayabilirsiniz.  Kendi çevrimiçi pazarlama ağınızı geliştirebilirsiniz. Firmamız her zaman yeniliklerle güncellenecektir.  Sitemizde sunduğumuz Yol haritasını inceleyebilirsiniz. 
-BİRLİK OLALIM VE ŞİRKETİMİZİ GELİŞTİRELİM!.</p>
+            <h1>ABOUT</h1>
+            <h3>How to become a partner at TronGMC?</h3>
+
+            <ul>
+                <li>To become a partner of our company, you need to rent one of the packages we offer.  So you support Tron Blockchain and become a partner of our company.</li>
+            </ul>
+            <h3>What should happen after registering with the company?</h3>
+            <ul>
+                <li>After registering with our company, you must be a member of the VIP packages we offer.  When a member is found, machines are rented, earnings are protected online and reflected in your balance.</li>
+                <li>Rented machines work according to the amount you pay and profit is provided.</li>
+            </ul>
+
+            <h3>I want to withdraw money from my account, how can I do it?</h3>
+            <ul>
+                <li>Go to the Accounts section and run the "Withdrawal" section.  and enter the trx address.  Enter the limits you want to shoot and shoot.</li>
+                <li>The minimum withdrawal deposit is 10 trx, the minimum savings deposit is 20 trx.</li>
+                <li>You can withdraw money only once a day.  Withdrawal and investment hours are 08:00 and 20:00.  (Swedish time)</li>
+            </ul>
+
+            <h3>Which deposit units can he invest in?</h3>
+            <ul>
+                <li> TRX (TRC20) only.  Because we have an agreement with Tron company and we only accept tron.  This is because to support Tron Blockchain and build volume on Tron.</li>
+            </ul>
+
+            <h3>What is the level of VIP packages?</h3>
+            <ul>
+                <li>  Each VIP package has different devices and different hash rate.  By purchasing VIP1, you are renting your minimum power consumption.  and increases the weak hashrate size by 7% per day.  However, by purchasing VIP 5 you get a higher interest rate, which is fair.</li>
+            </ul>
+
+            <h3>I have withdrawn money but it hasn't come yet, why?</h3>
+            <ul>
+                <li>There may be delays in the withdrawal time.  The reason for this is that it is registered with a company owned by more people.  If you have such a letter, please send a text message to our support line.</li>
+                <li>Only TRX is used for withdrawals.</li>
+                <li>Your transactions are recorded and you can see them.</li>
+            </ul>
+
+            <h3> How can I find out or change my password if I forgot it?</h3>
+            <ul>
+                <li>
+                    If you want to change the password, you can update the password by using "Change password" in the accounts section and typing the previous password and re-entering the new password and new password.
+                </li>
+            </ul>
+
+            <h3>How is money deposited into the account?</h3>
+            <ul>
+                <li> If you want to add funds to your account, go to "profile" and tap on deposit.  after that select TRX (TRC20) and choose how much you want to invest.  (minimum deposit 10 trx. Commission 2% ) Trx amount will be transferred to your balance after saving.</li>
+            </ul>
+
+            <h3>How much can I get from VIP packages?</h3>
+            <ul>
+                <li>VIP-1=10 purchase rights,</li>
+                <li>VIP-2=50 purchase rights,</li>
+                <li>VIP-3=100 purchase rights,</li>
+                <li>VIP-4=500 purchase right,</li>
+                <li>VIP-5=1000 purchase rights.</li>
+            </ul>
+
+            <p>The reason we limit packages here is because machines are limited.  As our company grows, you can choose more machines.
+            The dashboard will be updated as other packages arrive.</p>
+                <br>
+            <p>We kindly ask you to follow the rules set out below.  Otherwise, your account will be blocked.</p>
+
+            <ul>
+                <li>Opening multiple accounts from the same IP,</li>
+                <li> Interfering with someone else's account, hacking their account and changing their password.</li>
+                <li>BOT, Program usage in the account.</li>
+                <li>Logging into user accounts or asking users to take over accounts.</li>
+                <li>Disrespecting and insulting moderators.</li>
+            </ul>
+
+            <br>
+            <p> These rules are important and basic rules for our company.  We care about our users and we love them.</p>
+            <br>
+            <p>I have some questions, where can I find the answers to these questions?</p>
+            <br>
+            <ul>
+                <li>If you have questions, go to the link in the "Support" section above and write your questions to the moderators.  They will answer your related questions.</li>
+            </ul>
+            <br>
+            <p>Spinner system and VIP packages</p>
+            <br>
+            <ul>
+                <li>VIP-1= 7 days and 7% gain.  1 reel spin gift.</li>
+                <li>VIP-2=10 days and 8% gain.  3 reel spins gift.</li>
+                <li>VIP-3= 15 days and 10% gain.  6 reel spins gift.</li>
+                <li>VIP-4= 20 days and 11% profit.  10 spins, 50 tron ​​gift.</li>
+                <li>VIP-5=25 days and 12% gain.  20 spins, 250 trx gifts.</li>
+            </ul>
+            <br>
+            <p> "SPIN THE WHEEL" section is a bonus system created for fun.  Our users can earn additional income by taking advantage of this wheel.  The wheel spins randomly.</p>
+
+            <div id="img_about_box"><img src="@/assets/images/license.jpg" alt="" id="about1"></div>
+            <p>Tron Genesis Mining Cloud is officially licensed by Justin Sun.  We are grateful to Justin Sun for this license.</p>
+
+            <h3>Promotional Reward system</h3>
+            <ul>
+                <li>You can earn additional rewards by inviting your friends to Tron GMC.  10% in 1st Referral, 2.Referaldd 5% In the 3rd Referral you gain 3%.</li>
+                <li>For example, an invitee named A receives B and A receives 10% of B's ​​deposit. If B goes and invites C, this time A gets 5% of C's investment. If person C invites person D, this time person A wins 3% of the investment amount.</li>
+                <li>To create a team, copy the "Share with friends" link and send it to your friends.  After your friends register and purchase the VIP package, the promotional reward will be automatically reflected in your balance.</li>
+                <li> You get more income by referring people to the company.  You also support the company. In return for this support, you will receive certain bonuses.  As TRONGMC, we send our best wishes, love and respect.</li>
+            </ul>
+            <h3> Lucky Tickets draws</h3>
+            <ul>
+                <li>These sweepstakes are a new method implemented by TronGMC and welcomed by users.  You can buy lottery tickets here and increase your chances of winning. The first 3 randomly selected participants are awarded prizes, taking into account the number of tickets and the number of referrals.  (The prizes will be different in each draw.) The ticket price is 5 trx.</li>
+            </ul>
+                <br>
+                <h4> Official Information of the Company</h4>
+                <ul id="link_box">
+                    <li> <a href="https://www.trongmc.com/" class="link_color">Official website</a></li>
+                    <li> <a href="https://t.me/Trongmc_company" class="link_color">Official Telegram channel</a></li>
+                    <li><a href="https://t.me/+24Hg5OYpZk9mMWQx" class="link_color">Official Telegram group </a></li>
+                    <li>Company address: TOPSECRET company established in Sweden.</li>
+
+                </ul>
+                <br/><br/><br/>
+                <p> Tron Genesis Mining Cloud is officially licensed by Justin Sun.  We are grateful to Justin Sun for this license.</p>
+           
+            <br><p>ABOUT TRON GENESIS MINING CLOUD COMPANY</p><br><br>
+            <p> Tron Genesis Mining Cloud company is Tron mining company which was established on 05.10.2021 in İşvecre.  Our company's goal is to minimize the Gwei fee on the Tron blockchain.  We are working hard for this.  In short, TRONGMC company is signing a brand new plan!.  We are here with our company's online cloud platform on 20.07.2023.  Our company's purpose is for people to rent machinery for Mining Online and support the Tron blockchain.  With this, Gwei will minimize fees and create a faster blockchain with less fees!  In this, the machines you rent will provide you with additional income.  For example, when you buy Vip1, the special software will be preparing the X brand machine in our company for your system.  So you will be able to mine online with Cloud Mining.  We have made promotional rewards more profitable for you.  so by bringing more customers to our company, you will support us and yourself.  You can profit by trusting yourself and us, and you can start with the VIP packages offered by our company.  You can develop your own online marketing network.  Our company will always be updated with innovations.  You can review the Roadmap we offer on our site.</p>
+            <br><p>LET'S BE TOGETHER AND DEVELOP OUR COMPANY!.</p>
         </div>
     </main>
 </template>
@@ -19,7 +136,7 @@ main {
     background: #111;
     display: flex;
     justify-content: center;
-    align-items: center;
+    /* align-items: center; */
 }
 
 h1{
@@ -27,5 +144,34 @@ h1{
     align-items: center;
     justify-content: center;
     background-color: gray;
+}
+.container{
+    padding: 2em;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+ul,li,p{
+    line-height: 1.5em;
+}
+h3{
+    padding: .7em 0px;
+}
+h4{
+    line-height: 2em;
+}
+#img_about_box{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 1em;
+
+}
+#about1{
+    width: 90%;
+    object-fit: cover;
+
+    
+}
+.link_color{
+    color: #8888ff;
 }
 </style>
